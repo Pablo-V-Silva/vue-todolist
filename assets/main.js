c6d5f693be301b94919c7f87bf20ac7380767c0d
Pablo-V-Silva <booleanpablosilva@gmail.com>
2021-11-02 14:59:05 +0100

    add logic of js for add tasks and remove it by his n

--- assets/main.js
+++ assets/main.js
@@ -1,16 +1,28 @@
 const app = new Vue({
   el: '#app',
   data: {
-    tasks: [
-      'prova',
-      'prova',
-      'prova'
-    ],
+    tasks: [],
     logo: 'https://www.boolean.careers/images/misc/logo.png',
     newTask: '',
     nullTask: false,
     caracterTask: false
   },
   methods: {
+    addTask() {
+      if (this.newTask != '' && this.newTask.length > 8) {
+        this.tasks.push(this.newTask)
+        nullTask = false
+        caracterTask = false
+      } else if (this.newTask.length < 5) {
+        this.caracterTask = true
+      } else {
+        this.nullTask = true
+      }
+      this.newTask = ''
+    },
+
+    removeTask(i) {
+      this.tasks.splice(i, 1)
+    }
   }
 })
