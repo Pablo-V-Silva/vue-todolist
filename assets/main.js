415facadba3cc0972029115f96515bfabf41bd13
Pablo-V-Silva <booleanpablosilva@gmail.com>
2021-11-02 12:29:30 +0100

    Header and part of main done

---
+++ assets/main.js
@@ -0,0 +1,13 @@
+const app = new Vue({
+  el: '#app',
+  data: {
+    tasks: [],
+    logo: 'https://www.boolean.careers/images/misc/logo.png',
+    newTask: '',
+    nullTask: false,
+    caracterTask: false
+  },
+  methods: {
+
+  }
+})
